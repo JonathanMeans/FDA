0b372977612fdb3a644d7bc4c3391f283abba66d
Jonathan E. T. Means <jetmeans@gmail.com>
2014-11-02 07:29:24 -0800

    Test commit

--- FDA/src/fda/FDA.java
+++ FDA/src/fda/FDA.java
@@ -5,48 +5,14 @@
  */
 package fda;
 
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-
 /**
  *
  * @author paulcorter
  */
-public class FDA extends Application {
+public class FDA {
     
-    @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-                System.out.println("Hello Japan!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        launch(args);
+        System.out.println("Hello Worlds!");
     }
     
 }
