ea62eee2ae8da1dd4dbf69e07594d5bed9e6409f
Jonathan E. T. Means <jetmeans@gmail.com>
2014-11-08 22:11:15 -0800

    Add method to check url type

--- FDA/src/fda/FDA.java
+++ FDA/src/fda/FDA.java
@@ -5,11 +5,54 @@
  */
 package fda;
 
+import org.jfree.chart.demo.BarChartDemo1;
+import org.jsoup.Jsoup;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
 /**
  *
  * @author paulcorter
  */
 public class FDA {
+
+    //Used for URL validation
+    public enum UrlType{FANDOM, FIC, BETA, UNKNOWN}
+
+    /* Take String representation of a URL and determine what it is meant to lead to
+     *
+     * @param url the url to be tested
+     * @return the corresponding UrlType
+     */
+    public static UrlType checkUrl(String url) {
+        String baseUrl = "https://www.fanfiction.net/";
+        String ficString = baseUrl + "s/\\d+/\\d+/.+";
+        String betaString = baseUrl + "betareaders/.+";
+        String fandomString = baseUrl + ".+";
+
+        //Test whether URL matches the fic pattern
+        Pattern storyPattern = Pattern.compile(ficString);
+        Matcher storyMatcher = storyPattern.matcher(url);
+        if (storyMatcher.find()) {
+            return UrlType.FIC;
+        }
+
+        //Test whether URL matches the beta listing pattern
+        Pattern betaPattern = Pattern.compile(betaString);
+        Matcher betaMatcher = betaPattern.matcher(url);
+        if (betaMatcher.find()) { return UrlType.BETA; }
+
+        //Test whether URL matches the fandom pattern
+        Pattern fandomPattern = Pattern.compile(fandomString);
+        Matcher fandomMatcher = fandomPattern.matcher(url);
+        if (fandomMatcher.find()) { return UrlType.FANDOM; }
+
+        return UrlType.UNKNOWN;
+
+    }
+
     /* Added comment to test pushing to the Git repository */
     public static void main(String[] args) {
         System.out.println("Hello Worlds!");
