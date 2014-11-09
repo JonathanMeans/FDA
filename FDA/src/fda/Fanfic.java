cda784ac020eba50f833c9363370ca9309bd073a
Jonathan E. T. Means <jetmeans@gmail.com>
2014-11-08 23:03:06 -0800

    Create Fanfic class

---
+++ FDA/src/fda/Fanfic.java
@@ -0,0 +1,29 @@
+package fda;
+
+import java.util.Date;
+
+/**
+ * This class represents a fic
+ * Methods include setters and getters for each field
+ */
+public class Fanfic {
+    private String title;
+    private String ficUrl;
+    private String author;
+    private String authorUrl;
+    private String coverUrl;
+    private String summary;
+    private String rating;
+    private String language;
+    private String[] genres;
+    private int chapters;
+    private int words;
+    private int favorites;
+    private int reviews;
+    private int follows;
+    private Date publishedDate;
+    private Date updatedDate;
+    private String[] characters;
+    private String[][] pairings;
+    private double popularity;
+}
