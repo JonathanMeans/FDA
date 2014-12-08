1fcf94140be305a3a9c1d1fec3484d628e2aa5f2
Karolina Reitz <klegwant@gmail.com>
2014-12-07 19:29:02 -0500

    Added beta sorting by popularity and a progress bar for the beta reader scraping.

--- FDA/src/fda/Beta.java
+++ FDA/src/fda/Beta.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
  * This class represents a FanFiction Beta Reader.
  * Author: Karolina Reitz
  */
-public class Beta {
+public class Beta implements Comparable<Beta> {
 	private String name = "";
 	private String betaUrl = "";
 	private String betaId = "";
@@ -14,7 +14,6 @@ public class Beta {
 	private String profile = "";
 	private String[] genres = null;
 	private int numStories = 0;
-	private String rating = "";
 	private double popularity = 0.0;
 	
 	public Beta() {
@@ -61,14 +60,6 @@ public class Beta {
 		this.profile = profile;
 	}
 
-	public String getRating() {
-		return rating;
-	}
-
-	public void setRating(String rating) {
-		this.rating = rating;
-	}
-
 	public String[] getGenres() {
 		return genres;
 	}
@@ -85,9 +76,19 @@ public class Beta {
 		this.numStories = numStories;
 	}
 
-	public double calculatePopularity() {
+	public double getPopularity() {
 		return popularity;
-	}	
+	}
+	
+	public void setPopularity(double popularity) {
+		this.popularity = popularity;
+	}
+
+	@Override
+	public int compareTo(Beta o) {
+		// Reverse sort by priority (highest first)
+		return -1 * Double.compare(getPopularity(), o.getPopularity());
+	}
 
 	public String toString() {
 		StringBuffer buffer = new StringBuffer();
@@ -95,8 +96,7 @@ public class Beta {
 		buffer.append("Name: " + getName() + "\n");
 		buffer.append("Num Stories: " + getNumStories() + "\n");
 		buffer.append("Join Date: " + getJoinDate() + "\n");
-		buffer.append("Popularity: " + calculatePopularity() + "\n");
-		buffer.append("Rating: " + getRating() + "\n");
+		buffer.append("Popularity: " + getPopularity() + "\n");
 		buffer.append("Profile: " + getProfile() + "\n");
 		buffer.append("Genres: " + Arrays.toString(getGenres()));
 		
--- FDA/src/fda/BetaScraper.java
+++ FDA/src/fda/BetaScraper.java
@@ -1,8 +1,16 @@
 package fda;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Scanner;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -11,24 +19,33 @@ import org.jsoup.select.Elements;
 
 /**
  * This class scrapes Beta Reader information from FanFiction.net and creates
- * Beta objects.
- * Author: Karolina Reitz
+ * Beta objects. Author: Karolina Reitz
  */
-public class BetaScraper extends Scraper {
+public class BetaScraper extends Scraper implements PropertyChangeListener {
 	public static final String FANFICTION_BASE_URL = "https://www.fanfiction.net/";
 	public static final int DEFAULT_PAGES_TO_DOWNLOAD = 1;
+	private static final int BETAS_PER_PAGE = 51;
+	private static ProgressDialog progressDialog = null;
+	private static int currentProgressValue = 0;
+	private static int totalProgressTasks = 0;
 
 	public BetaScraper() {
+
+	}
+
+	@Override
+	public void propertyChange(PropertyChangeEvent event) {
 
 	}
 
 	/**
 	 * Main entry point - given a url to a Beta readers page on fanfiction.net,
 	 * this will download the beta information, parse it into Beta objects, and
-	 * return a List of the Beta objects.  This will download the default number
+	 * return a List of the Beta objects. This will download the default number
 	 * of beta reader pages for the given URL (default is 1).
 	 * 
-	 * @param url The URL for the Beta readers page on fanfiction.net
+	 * @param url
+	 *            The URL for the Beta readers page on fanfiction.net
 	 * @return a List of the parsed Beta objects
 	 * @throws IOException
 	 */
@@ -41,8 +58,10 @@ public class BetaScraper extends Scraper {
 	 * this will download the beta information, parse it into Beta objects, and
 	 * return a List of the Beta objects.
 	 * 
-	 * @param url The URL for the Beta readers page on fanfiction.net
-	 * @param numPages The number of Beta pages to download
+	 * @param url
+	 *            The URL for the Beta readers page on fanfiction.net
+	 * @param numPages
+	 *            The number of Beta pages to download
 	 * @return a List of the parsed Beta objects
 	 * @throws IOException
 	 */
@@ -50,16 +69,37 @@ public class BetaScraper extends Scraper {
 			throws IOException {
 		List<Beta> betas = new ArrayList<Beta>();
 
+		// Create a progress bar dialog to communicate the progress to the user
+		progressDialog = new ProgressDialog();
+		currentProgressValue = 0;
+
+		// Calculate the total number of tasks represented by the progress bar
+		totalProgressTasks = numPages * BETAS_PER_PAGE;
+
 		// Parse the given number of beta reader pages
 		for (int i = 1; i <= numPages; i++) {
-			parseBetasPage(url, i, betas);
+			// Parse the next beta readers page
+			try {
+				parseBetasPage(url, i, betas);
+			} catch (IOException ioe) {
+				// Clean up the progress bar
+				progressDialog.dispose();
+				throw ioe;
+			}
 		}
+
+		// Sort the list of betas
+		Collections.sort(betas);
+
+		// Dispose of the progress bar dialog since we're done
+		progressDialog.dispose();
 
 		return betas;
 	}
 
 	/*
-	 * This function parses a single beta readers page and stores the Beta objects in the given List.
+	 * This function parses a single beta readers page and stores the Beta
+	 * objects in the given List.
 	 */
 	private static void parseBetasPage(String url, int pageNum, List<Beta> betas)
 			throws IOException {
@@ -73,32 +113,47 @@ public class BetaScraper extends Scraper {
 			Element betaLink = betaCell.child(0);
 			String betaHref = betaLink.attr("href");
 			Beta beta = new Beta();
-			beta.setBetaUrl(FANFICTION_BASE_URL + betaHref.substring(1));
-			
+			beta.setBetaUrl(createAuthorUrl(betaHref.substring(1)));
+
 			String[] tokens = betaHref.split("\\/");
-			if(tokens.length < 3) {
+			if (tokens.length < 3) {
 				System.err.println("Error parsing beta URL");
 				return;
 			}
 			beta.setBetaId(tokens[2]);
 			beta.setName(betaLink.ownText());
 			Elements spans = betaLink.select("span[title=stories]");
-			if(spans.size() == 0) {
-				System.err.println("Error parsing numStories for the Beta reader");
+			if (spans.size() == 0) {
+				System.err
+						.println("Error parsing numStories for the Beta reader");
 				return;
 			}
 			beta.setNumStories(Integer.parseInt(spans.get(0).ownText()));
 			spans = betaLink.select("span[title=join date]");
 			beta.setJoinDate(spans.get(0).ownText());
-			
+
 			// Download the beta's profile page to get more information
-			downloadProfile(beta);
+			// downloadProfile(beta);
+			downloadAuthorPage(beta);
 			betas.add(beta);
+
+			// Update the progress bar
+			currentProgressValue++;
+			progressDialog.setValue((100 * currentProgressValue)
+					/ totalProgressTasks);
+			// System.out.println((100 * currentProgressValue) /
+			// totalProgressTasks);
+			// progressDialog.invalidate();
 		}
 	}
 
+	private static String createAuthorUrl(String url) {
+		return FANFICTION_BASE_URL + url.replace("beta", "u");
+	}
+
 	/*
-	 * This function builds the URL for the given page number with the given base URL.
+	 * This function builds the URL for the given page number with the given
+	 * base URL.
 	 */
 	private static String getPageUrl(String baseUrl, int pageNum) {
 		if (pageNum == 1) {
@@ -109,7 +164,8 @@ public class BetaScraper extends Scraper {
 	}
 
 	/*
-	 * This function downloads the given Beta's profile and adds the information to the object.
+	 * This function downloads the given Beta's profile and adds the information
+	 * to the object.
 	 */
 	private static void downloadProfile(Beta beta) throws IOException {
 		// Download the profile page
@@ -153,15 +209,91 @@ public class BetaScraper extends Scraper {
 		beta.setGenres(genresArr);
 	}
 
+	private static void downloadAuthorPage(Beta beta) throws IOException {
+		// Download the profile page
+		// System.out.println("Downloading " + beta.getBetaUrl());
+		Document doc = null;
+		// try {
+		doc = Jsoup.connect(beta.getBetaUrl()).get();
+		// } catch (IOException ioe) {
+		// // Try one more time to see if this was just a network error
+		// doc = Jsoup.connect(beta.getBetaUrl()).get();
+		// }
+
+		// Pull out the beta's profile
+		double popularitySum = 0.0;
+		Elements stories = doc.select("div[class=z-padtop2 xgray");
+		for (int i = 0; i < stories.size(); i++) {
+			Element story = stories.get(i);
+			Fanfic fic = createFic(story);
+			popularitySum += fic.getPopularity();
+		}
+
+		// A beta's popularity is the average popularity of their stories
+		double popularity = popularitySum / (double) stories.size();
+		beta.setPopularity(popularity);
+	}
+
+	private static Fanfic createFic(Element story) {
+		Fanfic fic = new Fanfic();
+
+		String text = story.ownText();
+
+		int favsPos = text.indexOf("Favs: ");
+		if (favsPos >= 0) {
+			String favsText = text
+					.substring(favsPos + 6, text.indexOf("-", favsPos + 1))
+					.replaceAll(",", "").trim();
+			try {
+				fic.setFavorites(Integer.parseInt(favsText));
+			} catch (NumberFormatException nfe) {
+				System.err.println("***** Error parsing input: '" + text + "'");
+				nfe.printStackTrace();
+			}
+		}
+
+		Elements publishedData = story.select("span");
+
+		if (publishedData.size() == 2) {
+			// The first is the updated date, the second is the published date
+			String publishedString = publishedData.get(0).attr("data-xutime");
+			fic.setUpdatedDate(new Date(Long.parseLong(publishedString) * 1000L));
+		}
+
+		// Now extract the published date
+		String publishedString = publishedData.get(publishedData.size() - 1)
+				.attr("data-xutime");
+		fic.setPublishedDate(new Date(Long.parseLong(publishedString) * 1000L));
+
+		// If there's no updated date, set the updated date to be the published
+		// date
+		if (fic.getUpdatedDate() == null) {
+			fic.setUpdatedDate(fic.getPublishedDate());
+		}
+
+		return fic;
+	}
+
+	public static void test(String url) throws Exception {
+		String out = new Scanner(new URL(url).openStream(), "UTF-8")
+				.useDelimiter("\\A").next();
+		System.out.println(out);
+	}
+
 	public static void main(String[] args) {
 		try {
-			List<Beta> betas = extractBetas("https://www.fanfiction.net/betareaders/book/Harry-Potter/", 1);
+			List<Beta> betas = extractBetas(
+					"https://www.fanfiction.net/betareaders/book/Harry-Potter/",
+					3);
 			int index = 0;
 			for (Beta beta : betas) {
 				index++;
-				System.out.println(index + ": " + beta.toString());
+				System.out.println(index + ": " + beta.getName()
+						+ " popularity = " + beta.getPopularity()
+						+ " numStories = " + beta.getNumStories());
 			}
-		} catch (IOException ioe) {
+			// test("https://www.fanfiction.net/betareaders/book/Harry-Potter/");
+		} catch (Exception ioe) {
 			ioe.printStackTrace();
 		}
 	}
